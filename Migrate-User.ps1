787a1c65e0621d30bf8d5747959ff82cb570953c
Myricht <kennethvanmele@hotmail.com>
2022-06-23 12:09:48 +0200

    Make sample of desktop.

--- Migrate-User.ps1
+++ Migrate-User.ps1
@@ -14,13 +14,31 @@ if (-not (Test-Connection -ComputerName $nPC -Count 1 -Quiet)){
 $userName = Read-Host -Prompt "User to migrate"
 #user test on old.
 if (-not (Test-Path -Path "\\$oPc\c$\users\$userName")){
-    #Throw "User $userName doesn't have an account on $oPC."    
+    Throw "User $userName doesn't have an account on $oPC."    
+}else{
+    $oldFolder = "\\$oPc\c$\users\$userName"
 }
+
 #user test on new.
 if (-not (Test-Path -Path "\\$nPc\c$\users\$userName")){
-    $cb = Read-Host -Prompt "User $userName doesn't have an account. Create Backup folder?"
-    if ($cb -eq "y" -or $cb -eq "yes"){
-        New-Item -Path "\\$nPC\c$\Backup_$userName" -ItemType Directory -Force
-        Write-Host "C:\\Backup_$userName created."
+    $prompt = Read-Host -Prompt "User $userName doesn't have an account. Create Backup folder?"
+    if ($prompt -eq "y" -or $prompt -eq "yes"){
+        New-Item -Path "\\$nPC\c$\Backup_$oPc_$userName" -ItemType Directory -Force
+        Write-Host "C:\Backup__$oPc_$userName created."
+        $newFolder = "\\$nPC\c$\Backup_$oPc_$userName"
+    }else{
+        $newFolder = "\\$nPc\c$\users\$userName"
+    }
+}
+
+#Desktop
+if (Test-Path -Path "$oldFolder\Desktop"){
+    $fs = ((Get-ChildItem -Path "$oldFolder\Desktop" -Recurse | Measure-Object -Property Length -Sum).Sum / 1Mb)
+    if ($fs -gt 0.001){
+        $fs = [math]::Round($fs, 2)
+        $prompt = Read-Host -Prompt "Desktop contains $fs Mbs of data. Migrate?"
+        if ($prompt -eq "y" -or $prompt -eq "yes"){
+            Robocopy.exe "$oldFolder\Desktop" "$newFolder\Desktop" /E
+        }
     }
 }
